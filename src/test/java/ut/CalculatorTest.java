53db64c2bfcb65e68457070d964369c70e2d9bea
Soumya27git <soumya_12015062@nitkkr.ac.in>
2024-09-18 14:32:32 +0530

    Add files via upload

---
+++ src/test/java/ut/CalculatorTest.java
@@ -0,0 +1,69 @@
+package ut;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.example.Calculator;
+
+public class CalculatorTest {
+
+	static Calculator cl;
+
+	@BeforeClass
+	public static void setUp() {
+		cl = new Calculator();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		System.gc();
+	}
+
+	@Test
+	public void testAdd() {
+
+		assertTrue(cl.doAdd(1, 2) == 3);
+	}
+
+	@Test
+	public void testSub() {
+		assertTrue(cl.doSub(1, 1) == 0);
+	}
+	
+//	@Test
+//	public void testSub2() {
+//		assertTrue(cl.doSub(-1,0) == -1);
+//	}
+
+	@Test
+	public void testMul() {
+		assertTrue(cl.doMul(9, 2) == 18);
+	}
+
+	@Test
+	public void testDiv() {
+		assertTrue(cl.doDiv(8, 2) == 4);
+	}
+	
+	@Test
+	public void testDiv2()
+	{
+		assertEquals(9.0,cl.doDiv(18, 2),.001);
+	}
+	
+	@Test
+	public void testDiv3()
+	{
+		assertEquals(36.00,cl.doMul(18, 2),0.00);
+	}
+	
+	@Test
+	public void testAdd2() {
+		assertNotEquals(35,cl.doAdd(20, 8));
+	}
+	
+
+}
